4a6e618e6b7c215fc4f1cf65e93623a0fd6a20e5
Miwth <thanhnnps28372@fpt.edu.vn>
2023-11-01 12:01:03 +0700

    Set up splash screen core

--- app/src/main/java/com/miwth/allure_spa/MainActivity.java
+++ app/src/main/java/com/miwth/allure_spa/MainActivity.java
@@ -1,14 +1,41 @@
 package com.miwth.allure_spa;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
+    Boolean splashLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow()
+                .setFlags(
+                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                );
+
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        loading();
+
+        splashScreen.setKeepOnScreenCondition(() -> {
+            return !splashLoaded;
+        });
+
+    }
+
+    private void loading() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashLoaded = true;
+                setContentView(R.layout.activity_main);
+            }
+        }, 3000);
     }
 }
